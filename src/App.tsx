5ce2a7647f20f287fbb09e7f15a99d722fb8af19
akmarallen <akmaralturdubaeva9@gmail.com>
2024-06-26 07:32:32 +0600

    save

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import "./App.css";
 import BookSearch from "./components/BookSearch/BookSearch";
 
 function App() {
+
   return (
     <>
       <h1>Book Search App</h1>
-      <BookSearch />
+      <BookSearch />/
     </>
   );
 }
--- src/components/BookSearch/BookSearch.tsx
+++ src/components/BookSearch/BookSearch.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import BookCard from "../BookCard/BookCard";
 import styles from "./BookSearch.module.scss";
 import { useState } from "react";
+import { log } from "console";
 
 interface IVolumeInfo {
   title: string;
@@ -18,65 +19,67 @@ interface IBook {
 }
 
 function BookSearch() {
-  const [query, setQuery] = useState<string>("");
-  const [books, setBooks] = useState<IBook[]>([]);
-  const [error, setError] = useState<string | null>(null);
+  // const [query, setQuery] = useState<string>("");
+  // const [books, setBooks] = useState<IBook[]>([]);
+  // const [error, setError] = useState<string | null>(null);
+  const baseUrl = import.meta.env.VITE_APP_BOOK_API;
 
   require("dotenv").config();
 
-  const apiKey = import.meta.env.VITE_APP_BOOK_API;
-  const baseUrl = import.meta.env.VITE_APP_BASE_URL;
-  async function searchBooks(query: string) {
-    const url = `${baseUrl}?q=${encodeURIComponent(query)}&key=${apiKey}`;
+  // const apiKey = import.meta.env.VITE_APP_BOOK_API;
 
-    try {
-      const response = await axios.get(url);
-      setBooks(response.data.items);
-    } catch (error) {
-      setError("Error fetching data");
-      console.error("Error fetching data:", error);
-    }
-  }
+  // async function searchBooks(query: string) {
+  // const url = `${baseUrl}?q=${encodeURIComponent(query)}&key=${apiKey}`;
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      searchBooks(query);
-    }
-  };
-
-  return (
-    <div className={styles.searchForm}>
-      <div className={styles.searchForm__form}>
-        <input
-          type="text"
-          value={query}
-          onKeyDown={handleKeyDown}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search for books"
-          className={styles.searchForm__form__input}
-        />
-        <button
-          onClick={() => searchBooks(query)}
-          className={styles.searchForm__form__button}
-        >
-          Search
-        </button>
-      </div>
-      {error && <p>{error}</p>} {}
-      <ul className={styles.searchForm__ul}>
-        {books.map(({ id, volumeInfo }: IBook) => (
-          <li key={id} className={styles.searchForm__ul__li}>
-            <BookCard
-              title={volumeInfo.title}
-              authors={volumeInfo.authors || []}
-              description={volumeInfo.description}
-              imageLinks={volumeInfo.imageLinks?.thumbnail}
-            />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  const axios = require("axios").default;
+  axios
+    .get(baseUrl)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
+
+// const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+//   if (event.key === "Enter") {
+//     searchBooks(query);
+//   }
+// };
+
+return (
+  <div className={styles.searchForm}>
+    <div className={styles.searchForm__form}>
+      <input
+        type="text"
+        value={query}
+        onKeyDown={handleKeyDown}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search for books"
+        className={styles.searchForm__form__input}
+      />
+      <button
+        onClick={() => searchBooks(query)}
+        className={styles.searchForm__form__button}
+      >
+        Search
+      </button>
+    </div>
+    {error && <p>{error}</p>} {}
+    <ul className={styles.searchForm__ul}>
+      {books.map(({ id, volumeInfo }: IBook) => (
+        <li key={id} className={styles.searchForm__ul__li}>
+          <BookCard
+            title={volumeInfo.title}
+            authors={volumeInfo.authors || []}
+            description={volumeInfo.description}
+            // imageLinks={volumeInfo.imageLinks?.thumbnail}
+          />
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 export default BookSearch;
